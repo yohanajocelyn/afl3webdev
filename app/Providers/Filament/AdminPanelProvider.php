e73ecfa9280d73cc9c4f76c1f94f2a337aad0911
Feli <feliciajoshlyn@gmail.com>
2025-05-21 11:02:04 +0700

    try start

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -36,6 +36,9 @@ class AdminPanelProvider extends PanelProvider
                 Pages\Dashboard::class,
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
+
+
+
             ->widgets([
                 Widgets\AccountWidget::class,
                 Widgets\FilamentInfoWidget::class,
---
+++ resources/views/components/my-courses.blade.php
@@ -0,0 +1,9 @@
+<x-layout>
+    <section class="bg-gray-100 min-h-screen flex flex-col items-center py-10">
+        <div class="mb-6 mt-6">
+        <h1 class="text-3xl font-bold text-gray-800">Kursus Saya</h1>
+        <p class="text-gray-600 mt-2">Lihat kursus yang anda ikuti</p>
+        
+    </div>
+    </section>
+</x-layout>
