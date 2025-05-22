7e75d67b1c0a327ecfb723155c8d73706b02b9bd
Feli <feliciajoshlyn@gmail.com>
2025-05-22 09:23:51 +0700

    Update workshop-detail.blade.php

--- resources/views/workshop-detail.blade.php
+++ resources/views/workshop-detail.blade.php
@@ -129,7 +129,7 @@
                                                             stroke-width="2"
                                                             d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
                                                     </svg>
-                                                    <span>{{ $meet->date }}</span>
+                                                    <span>{{ $meet->dateTime }}</span>
                                                 </div>
                                             </div>
                                         </div>
